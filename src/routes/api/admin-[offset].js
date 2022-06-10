152d7995d871e112a4e6e786e702b64c4509afec
Raspova <jonoelhen@gmail.com>
2022-06-10 09:14:48 -0400

    [ADD] eddit

--- src/routes/api/admin-[offset].js
+++ src/routes/api/admin-[offset].js
@@ -3,7 +3,7 @@ import 'dotenv/config'
 
 export async function get({params}) {
     const pool = new pg.Pool();
-    result =  await pool.query("SELECT * FROM student_contacts ORDER BY lastname LIMIT 100 OFFSET $1" , [ params.offset]);
+    let result =  await pool.query("SELECT * FROM student_contacts ORDER BY lastname LIMIT 100 OFFSET $1" , [ params.offset]);
     return {
         body: {
             results:result.rows
