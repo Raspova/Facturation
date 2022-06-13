00292d1dfa5dc8bc86c05e0681ad5920ec1a30c5
Raspova <jonoelhen@gmail.com>
2022-06-13 05:44:55 -0400

    [ADD] add

---
+++ src/routes/home.svelte
@@ -0,0 +1,98 @@
+<script>
+    import ButonCard from "../butonCard.svelte";
+    import SimplebuttonGreen from "../SimplebuttonGREEN.svelte";
+    import SimplebuttonRed from "../SimplebuttonRED.svelte";
+ 
+
+    //let pageIndex = 0;
+    let offset = 0;
+    let students = getStudents(offset);
+    //const pool = new pg.Pool();
+    
+    async function getStudents(offset) {
+        let studentsBuff =  await fetch("/api/admin-" + String(offset));
+        let res = await studentsBuff.json()
+        console.log(res)
+        return res.results;
+    }
+    
+        //pool.query("SELECT * FROM student_contacts ORDER BY lastname LIMIT 100 OFFSET " + offset)
+        //.then(res => (students = res.rows))
+       // const getAll  = "https://ext.edusign.fr/v1/student?page=";
+       // let studentsBuff = await fetch( getAll + String(pageIndex), {
+       //     headers: {
+       //         "Authorization": "Bearer " +  import.meta.env.VITE_TOKEN,
+       //         "Content-Type": "application/json"
+       //     }
+       // })
+       // let res = await studentsBuff.json()
+       // if (res.status == "error")
+       //     throw () => {}
+       // console.log(res)
+       // return res.result;
+    //}
+</script>
+    
+<svelte:head>
+  <title>Facturation Center</title>
+</svelte:head>
+
+<style global>
+
+h1 {
+        color: #253031;
+        border-radius: 5%;
+        margin-left: 10%;
+        /* color: #A9FFF7; */
+    }
+:global(body) {
+    margin: 10%;
+    min-height: 100vh;
+    background: #fff;
+}
+header {
+    width: 100%;
+    background-color: rgba(255, 255, 255, 0.899);
+    position:fixed;
+    top: 0;
+    left: 0;
+    display: grid;
+    grid-template-columns: 75% 15% 10%;
+    column-gap: 20px;
+}
+#nav  {
+    display: grid;
+    margin-top: 10px;
+}
+
+</style>
+
+
+<main>
+    <div>
+        {#await students}
+            <h2>LOADING</h2>    
+        {:then stuInfo}        
+            {#each  stuInfo as data}
+                <p><ButonCard data={data}></ButonCard></p>
+            {/each}
+        {:catch error}
+            {error}
+            <h1>No Pages Here (￣～￣;) </h1>
+        {/await}
+    </div>
+   
+</main>
+
+<header>
+    <h1>Facturation</h1> 
+    <nav>
+        <div id="nav">
+            <SimplebuttonGreen value="Next" on:click={() => { offset+= 100 ;students = getStudents(offset)}} ></SimplebuttonGreen>
+            {#if offset > 0}
+            <br>
+            <SimplebuttonRed value="Back" on:click={() => { offset-= 100 ;students = getStudents(offset)}} ></SimplebuttonRed>
+            {/if}
+        </div>
+    </nav>
+</header>
---
+++ src/routes/login.svelte
@@ -0,0 +1,225 @@
+<script>
+	import onMount from 'svelte';
+	export let _url;
+	let overlay;
+	
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,800');
+
+* {
+	box-sizing: border-box;
+}
+
+ 
+h1 {
+	font-weight: bold;
+	margin: 0;
+}
+
+ 
+p {
+	font-size: 14px;
+	font-weight: 100;
+	line-height: 20px;
+	letter-spacing: 0.5px;
+	margin: 20px 0 30px;
+}
+
+span {
+	font-size: 12px;
+}
+
+a {
+	color: #333;
+	font-size: 14px;
+	text-decoration: none;
+	margin: 15px 0;
+}
+
+button {
+	border-radius: 20px;
+	border: 1px solid #FF4B2B;
+	background-color: #FF4B2B;
+	color: #FFFFFF;
+	font-size: 12px;
+	font-weight: bold;
+	padding: 12px 45px;
+	letter-spacing: 1px;
+	text-transform: uppercase;
+	transition: transform 80ms ease-in;
+}
+
+button:active {
+	transform: scale(0.95);
+}
+
+button:focus {
+	outline: none;
+}
+
+button.ghost {
+	background-color: transparent;
+	border-color: #FFFFFF;
+}
+
+form {
+	background-color: #FFFFFF;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	padding: 0 50px;
+	height: 100%;
+	text-align: center;
+}
+
+input {
+	background-color: #eee;
+	border: none;
+	padding: 12px 15px;
+	margin: 8px 0;
+	width: 100%;
+}
+
+.container {
+	background-color: #fff;
+	border-radius: 10px;
+  	box-shadow: 0 14px 28px rgba(0,0,0,0.25), 
+			0 10px 10px rgba(0,0,0,0.22);
+	position: relative;
+	overflow: hidden;
+	width: 768px;
+	max-width: 100%;
+	min-height: 480px;
+}
+
+.form-container {
+	position: absolute;
+	top: 0;
+	height: 100%;
+	transition: all 0.6s ease-in-out;
+}
+.sign-in-container {
+	left: 0;
+	width: 50%;
+	z-index: 2;
+}
+.sign-up-container {
+	left: 0;
+	width: 50%;
+	opacity: 0;
+	z-index: 1;
+}
+@keyframes show {
+	0%, 49.99% {
+		opacity: 0;
+		z-index: 1;
+	}
+	
+	50%, 100% {
+		opacity: 1;
+		z-index: 5;
+	}
+}
+.overlay-container {
+	position: absolute;
+	top: 0;
+	left: 50%;
+	width: 50%;
+	height: 100%;
+	overflow: hidden;
+	transition: transform 0.6s ease-in-out;
+	z-index: 100;
+}
+
+
+@media ( max-width : 400px) {
+  .overlay-container{
+    /*display: none; */
+    width: 0;
+  }
+  .overlay-panel {
+    width: 200%;
+  }
+}
+
+.overlay {
+  background-image: url("Admin3.jpg");
+	/* background: #FF416C; */
+	/* background: -webkit-linear-gradient(to right, #FF4B2B, #FF416C); */
+	/* background: linear-gradient(to right, #FF4B2B, #FF416C); */
+	background-repeat: round;
+	background-size: contain;
+
+	/* background-position: 0 0; */
+	color: #FFFFFF;
+	position: relative;
+	/* left: -100%; */
+	height: 100%;
+	width: 200%;
+  	transform: translateX(0);
+	transition: transform 0.6s ease-in-out;
+}
+.overlay-panel {
+	position: absolute;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	padding: 0 40px;
+	text-align: center;
+	top: 0;
+	height: 100%;
+	width: 50%;
+	transform: translateX(0);
+	transition: transform 0.6s ease-in-out;
+}
+.overlay-left {
+	transform: translateX(-20%);
+}
+.overlay-right {
+	right: 0;
+	transform: translateX(0);
+}
+#hello {
+  position:absolute;
+  top: 4%;
+  left: 20%;
+  color: #333;
+}
+</style>
+
+
+<div class="container" id="container">
+	<div class="form-container sign-up-container">
+		<form action="#">
+			<h1>Create Account</h1>
+      <br>
+			<span>or use your email for registration</span>
+			<input type="text" placeholder="Name" />
+			<input type="email" placeholder="Email" />
+			<input type="password" placeholder="Password" />
+			<button>Sign Up</button>
+		</form>
+	</div>
+	<div class="form-container sign-in-container">
+		<form action="#">
+			<h1>Sign in</h1>
+      <br>
+			<input type="email" placeholder="Email" />
+			<input type="password" placeholder="Password" />
+			<a href="#">Forgot your password?</a>
+			<button>Sign In</button>
+		</form>
+	</div>
+	<div class="overlay-container">
+		<div class="overlay" bind:this={overlay}>
+			<!-- style="background-image: url('{_url}');" -->
+			<div class="overlay-panel overlay-right">
+				<!-- <h1 id="hello">Hello, Friend!</h1> -->
+			</div>
+		</div>
+	</div>
+</div>
